29b9170faf8d4358b2babd65d7e5108b84cc36d7
Koala Yeung <koalay@gmail.com>
2025-04-21 00:03:12 +0800

    context: various improvements
    * bug fix to the With functions return.
    * private context key instead of string.

--- context.go
+++ context.go
@@ -2,15 +2,23 @@ package authfully
 
 import "context"
 
+type contextKey int
+
+const (
+	contextKeyEnvironment contextKey = iota
+	contextKeyClient
+	contextKeyUser
+)
+
 // WithEnvironment is a function that stores the environment in the context.
-func WithEnvironment(ctx context.Context, env *Environment) {
+func WithEnvironment(ctx context.Context, env *Environment) context.Context {
 	// Store the environment in the context
-	ctx = context.WithValue(ctx, "environment", env)
+	return context.WithValue(ctx, contextKeyEnvironment, env)
 }
 
 // GetEnvironment retrieves the environment from the context.
 func GetEnvironment(ctx context.Context) *Environment {
-	env, ok := ctx.Value("environment").(*Environment)
+	env, ok := ctx.Value(contextKeyEnvironment).(*Environment)
 	if !ok {
 		return nil
 	}
@@ -18,14 +26,14 @@ func GetEnvironment(ctx context.Context) *Environment {
 }
 
 // WithClient is a function that stores the client in the context.
-func WithClient(ctx context.Context, client Client) {
+func WithClient(ctx context.Context, client Client) context.Context {
 	// Store the client in the context
-	ctx = context.WithValue(ctx, "client", client)
+	return context.WithValue(ctx, contextKeyClient, client)
 }
 
 // GetClient is a function that retrieves the client from the context.
 func GetClient(ctx context.Context) Client {
-	client, ok := ctx.Value("client").(Client)
+	client, ok := ctx.Value(contextKeyClient).(Client)
 	if !ok {
 		return nil
 	}
@@ -33,14 +41,14 @@ func GetClient(ctx context.Context) Client {
 }
 
 // WithUser is a function that stores the user in the context.
-func WithUser(ctx context.Context, user User) {
+func WithUser(ctx context.Context, user User) context.Context {
 	// Store the user in the context
-	ctx = context.WithValue(ctx, "user", user)
+	return context.WithValue(ctx, contextKeyUser, user)
 }
 
 // GetUser is a function that retrieves the user from the context.
 func GetUser(ctx context.Context) User {
-	user, ok := ctx.Value("user").(User)
+	user, ok := ctx.Value(contextKeyUser).(User)
 	if !ok {
 		return nil
 	}
